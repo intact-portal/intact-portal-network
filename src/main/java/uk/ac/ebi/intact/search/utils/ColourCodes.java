a23392e56b960310f4ecb349abb81f4f22b2536e
anjali <anjali@ebi.ac.uk>
2019-09-11 17:19:11 +0100

    NIV-326 : Species Color Code Changed

--- src/main/java/uk/ac/ebi/intact/search/utils/ColourCodes.java
+++ src/main/java/uk/ac/ebi/intact/search/utils/ColourCodes.java
@@ -5,15 +5,15 @@ package uk.ac.ebi.intact.search.utils;
  */
 public class ColourCodes {
 
-    public static final String SPECIES_TAXID_9606 = "rgb(255,0,0)"; // Homo Sapiens, Color: Red
-    public static final String SPECIES_TAXID_4932 = "rgb(148, 0, 211)"; // Mus musculus, Dark Violet
-    public static final String SPECIES_TAXID_10090 = "rgb(75, 0, 130)"; // Saccharomyces cerevisiae, Color : Indigo
-    public static final String SPECIES_TAXID_3702 = "rgb(0, 0, 255)"; // Arabidopsis thaliana (Mouse-ear cress),Color : Blue
-    public static final String SPECIES_TAXID_7227 = "rgb(0, 255, 0)"; // Drosophila melanogaster, Color: Green
-    public static final String SPECIES_TAXID_6239 = "rgb(0, 153, 153)"; // Caenorhabditis elegans, Color: Cyan
-    public static final String SPECIES_TAXID_562 = "rgb(255, 127, 0)"; // Escherichia coli, Color:Orange
-    public static final String SPECIES_TAXID_2 = "rgb(255,153,204)"; // chemical synthesis , Color: Pink
-    public static final String SPECIES_TAXID_OTHERS = "rgb(255,204,153)"; // Others, Color: Peach Orange
+    public static final String SPECIES_TAXID_9606 = "rgb(51, 94, 148)"; // Homo Sapiens, Color: Red
+    public static final String SPECIES_TAXID_4932 = "rgb(88, 115, 29)"; // Mus musculus, Dark Violet
+    public static final String SPECIES_TAXID_10090 = "rgb(107, 13, 10)"; // Saccharomyces cerevisiae, Color : Indigo
+    public static final String SPECIES_TAXID_3702 = "rgb(97, 74, 124)"; // Arabidopsis thaliana (Mouse-ear cress),Color : Blue
+    public static final String SPECIES_TAXID_7227 = "rgb(47, 132, 156)"; // Drosophila melanogaster, Color: Green
+    public static final String SPECIES_TAXID_6239 = "rgb(202, 115, 47)"; // Caenorhabditis elegans, Color: Cyan
+    public static final String SPECIES_TAXID_562 = "rgb(144, 163, 198)"; // Escherichia coli, Color:Orange
+    public static final String SPECIES_TAXID_2 = "rgb(141, 102, 102)"; // chemical synthesis , Color: Pink
+    public static final String SPECIES_TAXID_OTHERS = "rgb(173, 188, 148)"; // Others, Color: Peach Orange
     public static final String META_NODE = "rgb(220,220,220)";
 
 
